c814b673f721a6fbbb2c966318f64130ad7dfe22
talenaa <talenabarbosa7@gmail.com>
2025-01-29 11:14:31 +0100

    feat: finished the letter model

---
+++ app/Models/Letter.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Letter;
+use Illuminate\Database\Eloquent\Model;
+
+class Letter extends Model
+{
+    protected $fillable = [
+        'letter'
+    ];
+}
