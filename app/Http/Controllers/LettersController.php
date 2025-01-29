425a095f903dabf52e40001169742471c6d03688
talenaa <talenabarbosa7@gmail.com>
2025-01-29 11:29:13 +0100

    feat: finished letter controller

--- app/Http/Controllers/LettersController.php
+++ app/Http/Controllers/LettersController.php
@@ -6,5 +6,17 @@ use Illuminate\Http\Request;
 
 class LettersController extends Controller
 {
-    //
+    function assignLetterToNumber($numChain)
+{
+    if (strlen($numChain) != 8) {
+        return "Please, insert a valid number (8 numbers needed)";
+    }
+
+    $mod = (int)$numChain % 23;
+    $letter = Letter::find($mod + 1);
+
+    return $numChain . $letter->letter;
 }
+
+}
+
